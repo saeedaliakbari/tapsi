f5f729ce16ada3cd169bb465f67b79ffd6d3709c
Saeed <saliakbari6603@gmail.com>
2021-11-22 23:10:05 +0330

    add services , hero and header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".header__main ");
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".header__main ");
+// toggler.addEventListener("click", (e) => {
+//   navbar.classList.toggle("nav__expanded");
+// });
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+    //REMOVE PRIVIES ACTIVED CLASS
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+    //ADD NEW ACTIVE CLASS
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
 });
